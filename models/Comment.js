e0e41bef65d3acc00196c8b7bdabd5c28bacb06c
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-05-07 15:27:52 -0400

    Added models for Posts and Comments

--- models/Comment.js
+++ models/Comment.js
@@ -1,10 +1,46 @@
 const { Sequelize, Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/config");
 
-class Comment extends Model {}
+class Comment extends Model { }
 
 Comment.init(
-    // finish this Comment Model
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        contents: {
+            type: DataTypes.STRING,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id',
+            },
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'project',
+    }
 );
 
 module.exports = Comment;
